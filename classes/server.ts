47cd894ada9cf27bbeb7ff07c6148fda9dc5b255
Antonio Esteban Campuzano Guillén <antonioecampuzano@hotmail.com>
2019-04-12 00:31:04 +0200

    sección 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,22 +1,56 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
+import * as socket from '../sockets/socket';
 
-
+// servidor que levantamos.
 export default class Server {
+
+    private static _instance: Server;
 
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    // Patrón singleton
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private constructor() {
 
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
+
+        this.escucharSockets();
     }
 
-    start( callback: Function ) {
+    private escucharSockets() {
+        
+        console.log('Escuchando conexiones - sockets');
 
-        this.app.listen( this.port, callback );
+        this.io.on('connection', cliente => {
+
+            console.log('Cliente conectado');
+            
+            // Mensajes
+            socket.mensaje( cliente, this.io );
+            
+            // Desconectar
+            socket.desconectar(cliente);
+        });
+    }
+
+    start( callback: any /* Por algún motivo esto ya no va Function*/ ) {
+
+        this.httpServer.listen( this.port, callback );
     }
 }
 
